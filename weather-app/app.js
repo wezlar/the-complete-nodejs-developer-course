cc6d94def3255788c746e4acc0c29ab170c5729e
Dean Pugh <dean.pugh@15gifts.com>
2020-09-08 09:30:42 +0100

    More course work

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,6 +3,11 @@ const request = require('request');
 const url = 'http://api.weatherstack.com/current?access_key=6eead8b948e58809d57f8dcbea84be21&query=50.783132,0.313633&unit=m';
 
 request({ url, json: true }, (error, response) => {
+  if (error) {
+    console.log('Unable to connect to weather service.');
+    return;
+  }
+  
   const { temperature, feelslike, weather_descriptions } = response.body.current
   
   console.log(`${weather_descriptions[0]}. It is currently ${temperature} degrees outside, it feels like ${feelslike} degrees.`);
@@ -16,9 +21,4 @@ request({ url: mapboxUrl, json: true}, (error, response) => {
   const latitude = features[0].center[1];
 
   console.log(`${longitude} - ${latitude}`)
-})
-
-
-// geocoding
-// address
-// mapbox token pk.eyJ1IjoiZGVhbnB1Z2giLCJhIjoiY2tlb2Qya2tuMDEybDJybm80OXcwdTE3dSJ9.mRiM-3G2H5dO7agAtQ-IcQ
+});
