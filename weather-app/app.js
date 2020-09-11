be9aacc772ebdfb37ad26e2f46362e2ca740dcfd
Dean Pugh <dean.pugh@15gifts.com>
2020-09-11 16:53:12 +0100

    Latest of weather app work:

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,20 +5,25 @@ const url = 'http://api.weatherstack.com/current?access_key=6eead8b948e58809d57f
 request({ url, json: true }, (error, response) => {
   if (error) {
     console.log('Unable to connect to weather service.');
-    return;
+  } else if (response.body.error) {
+    console.log('Unable to find the location');
+  } else {
+    const { temperature, feelslike, weather_descriptions } = response.body.current
+    console.log(`${weather_descriptions[0]}. It is currently ${temperature} degrees outside, it feels like ${feelslike} degrees.`);
   }
-  
-  const { temperature, feelslike, weather_descriptions } = response.body.current
-  
-  console.log(`${weather_descriptions[0]}. It is currently ${temperature} degrees outside, it feels like ${feelslike} degrees.`);
 });
 
-const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGVhbnB1Z2giLCJhIjoiY2tlb2Qya2tuMDEybDJybm80OXcwdTE3dSJ9.mRiM-3G2H5dO7agAtQ-IcQ&limit=1';
+const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Dave%20Angeles.json?access_token=pk.eyJ1IjoiZGVhbnB1Z2giLCJhIjoiY2tlb2Qya2tuMDEybDJybm80OXcwdTE3dSJ9.mRiM-3G2H5dO7agAtQ-IcQ&limit=1';
 
 request({ url: mapboxUrl, json: true}, (error, response) => {
-  const { features } = response.body;
-  const longitude = features[0].center[0];
-  const latitude = features[0].center[1];
-
-  console.log(`${longitude} - ${latitude}`)
+  if (error) {
+    console.log('Unable to connecto to location service.');
+  } else if (!response.body.features.length) {
+    console.log('Unable to find location');
+  } else {
+    const { features } = response.body;
+    const longitude = features[0].center[0];
+    const latitude = features[0].center[1];
+    console.log(`${longitude} - ${latitude}`);
+  }
 });
