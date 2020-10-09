cbfee65e9497f3862b795cd8b0767664910e7137
Dean Pugh <dean.pugh@15gifts.com>
2020-10-09 15:39:58 +0100

    Video 41

---
+++ playground/6-raw-http.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const url = 'http://api.weatherstack.com/current?access_key=6eead8b948e58809d57f8dcbea84be21&query=45,-75&unit=m';
+
+const request = http.request(url, (response) => {
+  let data = '';
+
+  response.on('data', (chunk) => {
+    data = data + chunk.toString(); // converting buffer to a string
+    console.log(chunk);  
+  });
+
+  response.on('end', () => {
+    const body = JSON.parse(data);
+    console.log(body);
+  });
+});
+
+request.on('error', (error) => {
+  console.log('An error', error);
+});
+
+request.end()
