be5e379d1759623e5c8a62e58e87c9a9b3131d87
Dean Pugh <dean.pugh@15gifts.com>
2020-10-14 09:06:36 +0100

    Video 47 Add in basic templating

--- coursework/web-server/src/app.js
+++ coursework/web-server/src/app.js
@@ -3,7 +3,30 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Dean Pugh',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Dean Pugh',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    message: 'The help message goes here',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
