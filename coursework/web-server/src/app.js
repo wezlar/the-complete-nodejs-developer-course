75ca140c13682d43feee05bd63647e37d102a73d
Dean Pugh <dean.pugh@15gifts.com>
2020-10-16 15:15:32 +0100

    Video 48

--- coursework/web-server/src/app.js
+++ coursework/web-server/src/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
 
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
