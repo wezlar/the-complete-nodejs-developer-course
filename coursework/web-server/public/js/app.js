d93f942a6f9c84ab23053547c576109b765398a5
Dean Pugh <dean.pugh@15gifts.com>
2020-10-20 08:53:35 +0100

    Video 57

--- coursework/web-server/public/js/app.js
+++ coursework/web-server/public/js/app.js
@@ -1,12 +1,3 @@
-// const { response } = require("express")
-
-console.log('This is a javascript file')
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-  response.json().then((data) => {
-    console.log(data)
-  })
-})
 
 fetch('http://localhost:3000/weather?address=Eastbourne').then((response) => {
   response.json().then((data) => {
