ebf3a92a50c1bef4eca1804bbcbddcbf37e6dfe1
Dean Pugh <dean.pugh@15gifts.com>
2021-01-21 21:24:10 +0000

    Add in form and trigger api

--- coursework/web-server/public/js/app.js
+++ coursework/web-server/public/js/app.js
@@ -1,11 +1,19 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch('http://localhost:3000/weather?address=Eastbourne').then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error)
-    } else {
-      console.log(data.location)
-      console.log(data.forecast)
-    }
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value;
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error)
+      }
+      else {
+        console.log(data.location)
+        console.log(data.forecast)
+      }
+    })
   })
 })
