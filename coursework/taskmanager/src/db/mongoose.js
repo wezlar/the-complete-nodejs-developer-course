d733f692ee0cc3a857cde77778b390b04bd72095
Dean Pugh <dean.pugh@15gifts.com>
2021-02-05 20:45:35 +0000

    Video 83

---
+++ coursework/taskmanager/src/db/mongoose.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+const me = new User({
+  name: 'Dean',
+  age: 36,
+});
+
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((err) => {
+    console.log('Error!', err);
+  });
