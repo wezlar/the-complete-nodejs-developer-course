fddb3452f85907fe064c8475ba8f5a767feb7b79
Dean Pugh <dean.pugh@15gifts.com>
2021-02-07 20:55:32 +0000

    Video 89

--- coursework/taskmanager/src/db/mongoose.js
+++ coursework/taskmanager/src/db/mongoose.js
@@ -1,49 +1,9 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-});
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid');
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Password must not contain "password"');
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number');
-      }
-    },
-  },
 });
 
 // const me = new User({
@@ -72,15 +32,15 @@ const Tasks = mongoose.model('Tasks', {
   },
 });
 
-const task = new Tasks({
-  description: 'Paint the walls',
-});
+// const task = new Tasks({
+//   description: 'Paint the walls',
+// });
 
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
