577f2fbc9316f265d98c08ae9b8a02c7895a5efe
Dean Pugh <dean.pugh@15gifts.com>
2021-02-14 21:09:13 +0000

    Video 107

--- coursework/taskmanager/src/routers/user.js
+++ coursework/taskmanager/src/routers/user.js
@@ -8,7 +8,8 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -20,7 +21,8 @@ router.post('/users/login', async (req, res) => {
       req.body.email,
       req.body.password,
     );
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send();
   }
