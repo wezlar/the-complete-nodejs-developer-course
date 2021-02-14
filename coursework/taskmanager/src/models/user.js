072bf8a8318c2cbaffb4ee000b769332a3c958fb
Dean Pugh <dean.pugh@15gifts.com>
2021-02-14 20:45:14 +0000

    Video 105

--- coursework/taskmanager/src/models/user.js
+++ coursework/taskmanager/src/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -41,7 +42,22 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// middleware
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+// Has the plain text password before saving
 userSchema.pre('save', async function (next) {
   const user = this;
 
