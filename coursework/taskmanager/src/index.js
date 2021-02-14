d9ce1bfaaa9464ba631ab5ce9917aa2970e567da
Dean Pugh <dean.pugh@15gifts.com>
2021-02-14 20:55:49 +0000

    Video 106

--- coursework/taskmanager/src/index.js
+++ coursework/taskmanager/src/index.js
@@ -15,17 +15,15 @@ app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
 
-const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
 const myFunction = async () => {
-  const password = 'Red12345!';
-  const hashedPassword = await bcrypt.hash(password, 8);
+  const token = jwt.sign({ _id: 'abc123' }, 'secrettouse', {
+    expiresIn: '7 day',
+  });
+  console.log(token);
 
-  console.log(password);
-  console.log(hashedPassword);
-
-  const isMatch = await bcrypt.compare('Red12345!', hashedPassword);
-  console.log(isMatch);
+  const data = jwt.verify(token, 'secrettouse');
+  console.log(data);
 };
 
 myFunction();
