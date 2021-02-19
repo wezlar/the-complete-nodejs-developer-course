2d82bbaf6bff4aacc7576663d5202417c98714ed
Dean Pugh <dean.pugh@15gifts.com>
2021-02-19 15:57:02 +0000

    Video 124

--- coursework/taskmanager/src/index.js
+++ coursework/taskmanager/src/index.js
@@ -7,6 +7,30 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const multer = require('multer');
+const upload = multer({
+  dest: 'images',
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    // if (!file.originalname.endsWith('.pdf')) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error('Please upload a Word Document'));
+    }
+
+    cb(undefined, true);
+
+    // cb(new Error('File must be a PDF'))
+    // cb(undefined, true)
+    // cb(undefined, false)
+  },
+});
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+  res.send();
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
