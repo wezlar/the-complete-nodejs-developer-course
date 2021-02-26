9c6728138ac8b960e0fbe9c719f689c54aa3c42d
Dean Pugh <dean.pugh@15gifts.com>
2021-02-26 14:53:25 +0000

    Video 140

--- coursework/taskmanager/tests/math.test.js
+++ coursework/taskmanager/tests/math.test.js
@@ -1,4 +1,5 @@
 const {
+  add,
   calculateTip,
   fahrenheitToCelsius,
   celsiusToFahrenheit,
@@ -20,4 +21,23 @@ test('Should convert Fahrenheit to Celsius', () => {
 
 test('Should convert Celsius to Fahrenheit', () => {
   expect(celsiusToFahrenheit(0)).toBe(32);
+});
+
+// test('Async test demo', (done) => {
+//   setTimeout(() => {
+//     expect(1).toBe(2);
+//     done();
+//   }, 2000);
+// });
+
+test('Should add two numbers', (done) => {
+  add(2, 3).then((sum) => {
+    expect(sum).toBe(5);
+    done();
+  });
+});
+
+test('Should add two numbers async/await', async () => {
+  const sum = await add(10, 22);
+  expect(sum).toBe(32);
 });
