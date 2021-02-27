612053c2964db912234bff2e8581ee4e3659388c
Dean Pugh <dean.pugh@15gifts.com>
2021-02-27 20:12:49 +0000

    Video 148

--- coursework/taskmanager/tests/user.test.js
+++ coursework/taskmanager/tests/user.test.js
@@ -95,3 +95,37 @@ test('Should delete account for user', async () => {
 test('Should not delete user', async () => {
   await request(app).delete('/users/me').send().expect(401);
 });
+
+test('Should upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.avatar).toEqual(expect.any(Buffer));
+});
+
+test('Should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: 'Helen',
+    })
+    .expect(200);
+
+  const user = await User.findById(userOneId);
+  expect(user.name).toEqual('Helen');
+});
+
+test('Should not update invalid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: 'Eastbourne',
+    })
+    .expect(400);
+});
