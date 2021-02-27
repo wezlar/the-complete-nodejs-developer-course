39460643f3ab8919b455d5323c4b3d9c5339d2b4
Dean Pugh <dean.pugh@15gifts.com>
2021-02-27 21:33:29 +0000

    Video 149

--- coursework/taskmanager/tests/task.test.js
+++ coursework/taskmanager/tests/task.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest');
 const app = require('../src/app');
 const Task = require('../src/models/task');
-const { userOneId, userOne, setupDatabase } = require('./fixtures/db.js');
+const {
+  userOneId,
+  userOne,
+  userTwo,
+  taskOne,
+  setupDatabase,
+} = require('./fixtures/db.js');
 
 beforeEach(setupDatabase);
 
@@ -16,4 +22,25 @@ test('Should create task for user', async () => {
   expect(task).not.toBeNull();
 
   expect(task.completed).toEqual(false);
+});
+
+test('Should return correct users tasks', async () => {
+  const response = await request(app)
+    .get('/tasks')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.length).toBe(2);
+});
+
+test('Should not delete other users tasks', async () => {
+  const response = await request(app)
+    .delete(`/tasks/${taskOne._id}`)
+    .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  const task = await Task.findById(taskOne._id);
+  expect(task).not.toBeNull();
 });
