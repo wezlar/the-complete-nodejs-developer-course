2d0255f002a72fc93a3e91833ca4e7175b975c1e
Dean Pugh <dean.pugh@15gifts.com>
2021-03-03 20:09:09 +0000

    Video 155, 156 & 157

--- coursework/chat-app/src/index.js
+++ coursework/chat-app/src/index.js
@@ -12,8 +12,21 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('New websocket connection');
+
+  socket.emit('message', 'Welcome!');
+  socket.broadcast.emit('message', 'A new user has joined!');
+
+  // socket.emit('countUpdated', count);
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left.');
+  });
 });
 
 server.listen(port, () => {
